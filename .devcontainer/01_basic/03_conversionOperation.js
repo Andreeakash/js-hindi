64b7aef16e2b2550dc85ca702a5023c99c278650
Andree_akash <110694207+Andreeakash@users.noreply.github.com>
2024-05-01 16:59:26 +0000

    Conversion of data types

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,52 @@
+let score = 33
+
+let mohan = null
+
+let shyam = undefined
+
+let radha = "Krishna"
+
+let vat = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// console.log( typeof mohan);
+// let isValidnumber= Number(mohan);
+// console.log(isValidnumber);
+
+// console.log(typeof shyam);
+// let isValidnumber1= Number(shyam);
+// console.log(isValidnumber1);
+
+// console.log( typeof radha);
+// let isValidnumber2= Number(radha);
+// console.log(isValidnumber2);
+
+// console.log (typeof vat);
+// let isValidnumber3= Number(vat);
+// console.log(isValidnumber3);
+
+
+// "33" => 33
+// "33abs" => NaN( not an number)
+//true +> 1, false=> 
+
+// boolean conversion
+
+// let aman = ""
+// let aman = true;
+let aman = "Andree"
+
+let amanIsTrue = Boolean(aman);
+// console.log(amanIsTrue);
+
+// 1=> true, false= > 0
+// " ", empty string alwasys gives you zero where as  defined string gives you true value
+//" " => false, " Aman" => true
+
+let someNumber = 33
+let stingNUmber=String(someNumber);
+console.log(stingNUmber);
+console.log(typeof stingNUmber);
+
