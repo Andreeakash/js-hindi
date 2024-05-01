71742d341905feb84a80115bd6697f1a9f9f6794
Andree_akash <110694207+Andreeakash@users.noreply.github.com>
2024-05-01 16:29:06 +0000

    data types

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId =122554
+let accountEmail = "andree@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail="Andreeakash.com"
+accountPassword="12212112"
+accountCity="bnglr"
+
+// accountId = 2
+console.table([accountId,accountEmail,accountPassword,accountCity])
+console.log(accountId);
+
---
+++ .devcontainer/01_basic/02_variable.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as newer version
+
+// alert(3 +  3)   // we are using nodejs, not browser 
+
+// console.log("string");
+let name="aman"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+//primitive data types
+//number => 2 to the power of 53
+//big int
+//string = we use either sing quotes or double quotes
+// boolean => either true or false
+// null => representation of empty value / it is defined as empty
+// undefined=> where value is not defined
+// symbol => to find uniqueness
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof age);
+console.log(typeof "aman");
+console.log(typeof isLoggedIn);
+
+
