9574018c9e06d729d325540bc156af30829772df
Andree_akash <110694207+Andreeakash@users.noreply.github.com>
2024-05-02 08:51:14 +0000

    comparision with their data types

---
+++ .devcontainer/01_basic/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2 !=1);
+// console.log(2>=1);
+// console.log(2<1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// the reason is that ann equality check == and comparision >< >= <= work differently.
+//  comparision convert null to a number , treating it as 0. thats why (3) >=0 is true and (1) null>0 is false
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(ubdefined>=0);
+
+// === tripple equal check data types and  their value
+
+console.log("2"===2);// thats why this statement gives you false 
